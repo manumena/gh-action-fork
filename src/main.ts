2cbeb4be3a67f96244b7a52b213d0e94fa667ebe
Manuel Mena <manuelmena1993@gmail.com>
2022-05-09 16:47:37 -0300

    chore: try payload again

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
-import { Octokit } from "@octokit/rest"
+// import {Octokit} from '@octokit/rest'
 import {wait} from './wait'
 
-const octokit = new Octokit({})
+// const octokit = new Octokit({})
 
 async function run(): Promise<void> {
   try {
@@ -14,25 +14,27 @@ async function run(): Promise<void> {
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
 
-    // core.setOutput('time', new Date().toTimeString())
-
+    core.debug(JSON.stringify(github.context))
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     core.setOutput('payload', `The event payload: ${payload}`)
     core.setOutput('context', github.context)
 
     // Get latest release
-    // const latestRelease = octokit.rest.repos.getLatestRelease({
-    //   payload.owner.name,
-    //   payload.repository.name,
+    // TODO: make this more robust
+    // const latestRelease = await octokit.rest.repos.getLatestRelease({
+    //   owner: github.context.payload.owner.name,
+    //   repo: github.context.payload.repository?.name ?? ''
     // })
+    // core.setOutput('latestRelease', latestRelease.data.tag_name)
 
-    const latestRelease = await octokit.rest.repos.getLatestRelease({
-      owner: 'manumena',
-      repo: 'gh-action-fork'
-    })
-    // core.setOutput('latestRelease', JSON.stringify(latestRelease, undefined, 2))
-    core.setOutput('latestRelease', latestRelease.data.tag_name)
+    // Get diff between last tag and now
+    // const lastTag = latestRelease.data.tag_name
+    // const commits = await octokit.rest.repos.listCommits({
+    //   owner: payload.owner.name,
+    //   repo: payload.repository?.name ?? ''
+    // })
+    // core.setOutput('commits', commits)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
