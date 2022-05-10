948244541319458067015647b59c1ea781c62126
Manuel Mena <manuelmena1993@gmail.com>
2022-05-10 18:04:56 -0300

    chore: add auto bumping

--- dist/index.js
+++ dist/index.js
@@ -47,17 +47,18 @@ function run() {
             const payload = JSON.stringify(github.context.payload, undefined, 2);
             const owner = (_b = (_a = github.context.payload.repository) === null || _a === void 0 ? void 0 : _a.owner.name) !== null && _b !== void 0 ? _b : 'manumena';
             const repo = (_d = (_c = github.context.payload.repository) === null || _c === void 0 ? void 0 : _c.name) !== null && _d !== void 0 ? _d : 'gh-action-fork';
+            // TODO: fail if owner or repo are not filled properly
             core.setOutput('payload', `The event payload: ${payload}`);
             core.setOutput('context', github.context);
             // Get latest release
-            // TODO: make this more robust
             const latestRelease = yield octokit.rest.repos.getLatestRelease({
                 owner,
                 repo
             });
             core.setOutput('latestRelease', latestRelease.data.tag_name);
+            // TODO: fail if tag is not semver
             // Get diff between last tag and now
-            const lastTag = latestRelease.data.tag_name;
+            let lastTag = latestRelease.data.tag_name;
             const commitsData = yield octokit.rest.repos.compareCommits({
                 owner,
                 repo,
@@ -66,7 +67,40 @@ function run() {
             });
             // Extract messages
             const commitsMessages = commitsData.data.commits.map(commit => commit.commit.message);
-            core.setOutput('commits', commitsMessages);
+            // core.setOutput('commits', commitsMessages)
+            // Bump version depending on commit messages
+            let bumpPatch = false;
+            let bumpMinor = false;
+            let bumpMajor = false;
+            for (const message of commitsMessages) {
+                if (message.match('^(chore|docs|fix|refactor|revert|style|test): .+$')) {
+                    bumpPatch = true;
+                }
+                else if (message.match('^feat: .+$')) {
+                    bumpMinor = true;
+                }
+                else if (message.match('^break: .+$')) {
+                    bumpMajor = true;
+                }
+            }
+            const semverRegex = new RegExp('^([0-9]+).([0-9]+).([0-9]+)$', 'g');
+            const match = semverRegex.exec(lastTag);
+            let newTag = '';
+            if (match) {
+                if (bumpMajor) {
+                    const bump = (parseInt(match[1]) + 1).toString();
+                    newTag = lastTag.replace(semverRegex, `${bump}.$2.$3`);
+                }
+                else if (bumpMinor) {
+                    const bump = (parseInt(match[2]) + 1).toString();
+                    newTag = lastTag.replace(semverRegex, `$1.${bump}.$3`);
+                }
+                else if (bumpPatch) {
+                    const bump = (parseInt(match[3]) + 1).toString();
+                    newTag = lastTag.replace(semverRegex, `$1.$2.${bump}`);
+                }
+            }
+            core.setOutput('newTag', newTag);
         }
         catch (error) {
             if (error instanceof Error)
--- src/main.ts
+++ src/main.ts
@@ -10,17 +10,18 @@ async function run(): Promise<void> {
     const payload = JSON.stringify(github.context.payload, undefined, 2)
     const owner = github.context.payload.repository?.owner.name ?? 'manumena'
     const repo = github.context.payload.repository?.name ?? 'gh-action-fork'
+    // TODO: fail if owner or repo are not filled properly
 
-    core.setOutput('payload', `The event payload: ${payload}`)  
+    core.setOutput('payload', `The event payload: ${payload}`)
     core.setOutput('context', github.context)
 
     // Get latest release
-    // TODO: make this more robust
     const latestRelease = await octokit.rest.repos.getLatestRelease({
       owner,
       repo
     })
     core.setOutput('latestRelease', latestRelease.data.tag_name)
+    // TODO: fail if tag is not semver
 
     // Get diff between last tag and now
     const lastTag = latestRelease.data.tag_name
@@ -35,7 +36,38 @@ async function run(): Promise<void> {
     const commitsMessages = commitsData.data.commits.map(
       commit => commit.commit.message
     )
-    core.setOutput('commits', commitsMessages)
+    // core.setOutput('commits', commitsMessages)
+
+    // Bump version depending on commit messages
+    let bumpPatch = false
+    let bumpMinor = false
+    let bumpMajor = false
+    for (const message of commitsMessages) {
+      if (message.match('^(chore|docs|fix|refactor|revert|style|test): .+$')) {
+        bumpPatch = true
+      } else if (message.match('^feat: .+$')) {
+        bumpMinor = true
+      } else if (message.match('^break: .+$')) {
+        bumpMajor = true
+      }
+    }
+
+    const semverRegex = new RegExp('^([0-9]+).([0-9]+).([0-9]+)$', 'g')
+    const match = semverRegex.exec(lastTag)
+    let newTag = ''
+    if (match) {
+      if (bumpMajor) {
+        const bump = (parseInt(match[1]) + 1).toString()
+        newTag = lastTag.replace(semverRegex, `${bump}.$2.$3`)
+      } else if (bumpMinor) {
+        const bump = (parseInt(match[2]) + 1).toString()
+        newTag = lastTag.replace(semverRegex, `$1.${bump}.$3`)
+      } else if (bumpPatch) {
+        const bump = (parseInt(match[3]) + 1).toString()
+        newTag = lastTag.replace(semverRegex, `$1.$2.${bump}`)
+      }
+    }
+    core.setOutput('newTag', newTag)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
